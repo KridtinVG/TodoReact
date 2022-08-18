ae0e3572b4daa66ecaf5d75edceebc4965be1b9e
Kridtin <kridtin.v@gmail.com>
2022-08-18 15:42:25 +0700

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Keito Todolist
+          Learn React
         </a>
       </header>
     </div>
